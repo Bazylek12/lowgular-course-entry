ec94f45e01244f564b82a0aed5cb15858b6806f1
Bazylek12 <a.kosmowski7@gmail.com>
2022-11-27 14:24:02 +0100

    basic CRUD: remove employee

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Observable} from "rxjs";
-import {PersonModel} from "../model/person.model";
-import {CreateEmployeeModel} from "../model/create-employee.model";
+import {map, Observable} from 'rxjs';
+import { PersonModel } from '../model/person.model';
+import { CreateEmployeeModel } from '../model/create-employee.model';
 
 @Injectable()
 export class EmployeeService {
@@ -15,5 +15,10 @@ export class EmployeeService {
 
   create(employee: CreateEmployeeModel): Observable<any> {
     return this._httpClient.post('https://dummy.restapiexample.com/api/v1/create', employee);
+  }
+
+  delete(id: string): Observable<void> {
+    return this._httpClient.delete(`https://dummy.restapiexample.com/api/v1/delete/${id}`).pipe(map(_ => void 0));
+
   }
 }
--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -14,4 +14,10 @@ export class EmployeeListComponent {
   constructor(private _employeeService: EmployeeService) { }
   welcome: string = 'Hello world from property';
   data$: Observable<PersonModel[] | null> = this._employeeService.getAll();
+
+  remove(id: string) {
+    this._employeeService.delete(id).subscribe(() => {
+      alert('User was successfully removed');
+    });
+  }
 }
