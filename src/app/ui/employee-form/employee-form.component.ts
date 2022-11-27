4763401c25170b2026cc1911e3872ecc990c5e8e
Bazylek12 <a.kosmowski7@gmail.com>
2022-11-27 13:47:08 +0100

    basic CRUD: RXJS post with alert

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmployeeService } from '../../services/employee.service';
+import { CreateEmployeeModel } from '../../model/create-employee.model';
 
 @Component({
   selector: 'app-employee-form',
@@ -13,11 +15,17 @@ export class EmployeeFormComponent {
     age: new FormControl(null, [Validators.min(0)]),
     salary: new FormControl(null, [Validators.required, Validators.min(0)]),
   });
-  //
-  // onEmployeeFormSubmitted(employeeForm: FormGroup): void {
-  // }
 
-  onButtonClick(form: { name: string, age: number, salary: number}) {
-    alert(`User was successfully added to the database. Name: ${form.name}, Age ${form.age}, Salary ${form.salary}$`);
+  constructor(private _employeeService: EmployeeService) {
   }
+
+  onFormSubmitted(form: CreateEmployeeModel) {
+    this._employeeService.create({name: form.name, age: form.age, salary: form.salary}).subscribe((form) => {
+      alert(`User was successfully added to the database. Name: ${form.data.name}, Age ${form.data.age}, Salary ${form.data.salary}$`);
+    });
+  }
+
+  // onButtonClick(form: { name: string, age: number, salary: number }) {
+  //
+  // }
 }
