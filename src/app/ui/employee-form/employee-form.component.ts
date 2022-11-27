d10f07a1c2398fea06abdb7bee6753a28fac5e04
Bazylek12 <a.kosmowski7@gmail.com>
2022-11-27 12:45:10 +0100

    basic CRUD: alert with data from form

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-form',
@@ -16,4 +16,8 @@ export class EmployeeFormComponent {
   //
   // onEmployeeFormSubmitted(employeeForm: FormGroup): void {
   // }
+
+  onButtonClick(form: { name: string, age: number, salary: number}) {
+    alert(`User was successfully added to the database. Name: ${form.name}, Age ${form.age}, Salary ${form.salary}$`);
+  }
 }
